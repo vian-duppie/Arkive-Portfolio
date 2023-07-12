8db533ec694f8da6798b0e89d09aa66ce47e88ee
Vian du Plessis <125647194+vian-duppie@users.noreply.github.com>
2023-07-12 04:11:23 +0200

    Done

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { InputComponent } from './components/input/input.component';
+import { TestComponent } from './components/test/test.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    InputComponent,
+    TestComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule, 
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ frontend/src/app/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+// export const environment = {
+//     production: true,
+//     apiUrl: 'https://api.example.com',
+//     debugMode: false,
+//     // ... other environment-specific variables or configuration settings
+//   };
